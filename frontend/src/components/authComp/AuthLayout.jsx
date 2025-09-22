5dfe54df891d8d9ad3036bd0aceabc8829740327
Arihant Jain <152006522+jainArihant12@users.noreply.github.com>
2025-09-22 21:49:38 +0530

    Update AuthLayout.jsx

--- frontend/src/components/authComp/AuthLayout.jsx
+++ frontend/src/components/authComp/AuthLayout.jsx
@@ -8,8 +8,9 @@ const AuthLayout = () => {
   <div className='w-full md:w-1/2 flex justify-center items-center bg-black p-4 '>
     <h1 className='text-3xl md:text-4xl text-white text-center'>
       Welcome to Canteen ! 
+       <p className='text-2xl md:text-3xl text-white text-center'> (Wait for 10-20 second.Cold start Problem due to using free depolyment) </p>
     </h1>
-      <h1  className='text-2xl md:text-4xl text-white text-center'>(Wait for 10-20 second.Cold start Problem due to using free depolyment)</h1>
+
   </div>
 
   <div className='w-full md:w-1/2 flex justify-center items-center p-4 '>
